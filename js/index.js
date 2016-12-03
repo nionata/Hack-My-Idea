cf1648bfb3b39d18edff38659419f720ef6c34a6
Nicholas Ionata <n.ionata129@gmail.com>
2016-12-03 18:59:43 -0500

    Styled home page, added add page

---
+++ js/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $("#btn-connect").click(function() {
+        console.log("hi");
+        $("#idea").text("");
+        $("#pair-name").text("Nicholas Ionata");
+        $("#pair-email").text("n.ionata129@gmail.com");
+    });
+});
