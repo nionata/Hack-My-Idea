cd80cbb099a03f6bb9aae63fc50c8322f9c44e6c
Nicholas Ionata <n.ionata129@gmail.com>
2016-12-03 21:26:16 -0500

    Added cycling through ideas and connecting

--- js/index.js
+++ js/index.js
@@ -1,8 +1,41 @@
+var name;
+var email;
+var idea;
+var currentIdea = 1;
+
 $(document).ready(function() {
+    newIdea(currentIdea);
+
     $("#btn-connect").click(function() {
-        console.log("hi");
         $("#idea").text("");
-        $("#pair-name").text("Nicholas Ionata");
-        $("#pair-email").text("n.ionata129@gmail.com");
+        $("#pair-name").text(name);
+        $("#pair-email").text(email);
+    });
+
+    $("#btn-next").click(function() {
+       newIdea(currentIdea);
+    });
+
+    $("#submit").click(function() {
+        name = $("#name").value;
+        email = $("#email").value;
     });
 });
+
+function newIdea(number) {
+    $.getJSON("input.json", function(json) {
+        $("#pair-name").text("");
+        $("#pair-email").text("");
+        name = json[number]["name"];
+        email = json[number]["email"];
+        idea = json[number]["idea"]
+        $("#idea").text(idea);
+
+        if(currentIdea == Object.keys(json).length) {
+            currentIdea = 1;
+        } else {
+            currentIdea++;
+        }
+    });
+
+};
