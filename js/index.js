0c3c50cbda952a068b205aff572e5dd12151ef7a
Nicholas Ionata <n.ionata129@gmail.com>
2016-12-12 08:01:11 -0500

    Moved to one .js

--- js/index.js
+++ js/index.js
@@ -15,7 +15,34 @@ $(document).ready(function() {
     $("#btn-next").click(function() {
        newIdea(currentIdea);
     });
+
+    $("#form").on("submit", function() {
+
+        var enteredName = $("#name").val();
+        var newEmail = $("#email").val();
+        var newIdea = $("#idea").val();
+        var uId = randomNumer();
+        var db = firebase.database().ref("/ideas/");
+
+        db.child(uId + "/").set({
+            name: enteredName,
+            email: newEmail,
+            idea: newIdea
+        });
+
+        alert("Thank you, your idea is being added!");
+
+        setTimeout(function() {
+            window.location.replace("/index.html");
+        }, 1000);
+    });
 });
+
+function randomNumer() {
+    var max = 9999;
+    var min = 1000;
+    return Math.floor((Math.random() * (max - min) + min));
+};
 
 function newIdea(number) {
     $.getJSON("input.json", function(json) {
