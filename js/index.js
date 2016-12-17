b54b6d2854045a61350f952f00b44936e51392d4
Nicholas Ionata <n.ionata129@gmail.com>
2016-12-17 10:44:46 -0500

    Load now works

--- js/index.js
+++ js/index.js
@@ -6,7 +6,16 @@ var ideaList;
 var keys = [];
 
 $(document).ready(function() {
-    newIdea(currentIdea);
+
+    var db = firebase.database().ref("/ideas/");
+
+    db.on("value", function(snap) {
+        snap.forEach(function(childSnap) {
+            keys.push(childSnap.key);
+        });
+
+        newIdea(currentIdea);
+    });
 
     $("#btn-connect").click(function() {
         $("#idea").text("");
@@ -16,14 +25,6 @@ $(document).ready(function() {
 
     $("#btn-next").click(function() {
        newIdea(currentIdea);
-    });
-
-    var db = firebase.database().ref("/ideas/");
-
-    db.on("value", function(snap) {
-        snap.forEach(function(childSnap) {
-            keys.push(childSnap.key);
-        });
     });
 
     $("#form").on("submit", function() {
@@ -45,10 +46,7 @@ $(document).ready(function() {
 function newIdea(number) {
     var db = firebase.database().ref("/ideas/");
     //Child reference to the specific idea
-    var newIdeaRef = db.child("-KZ81Y2eSVpHV4A-jcof" + "/");
-
-    console.log(keys);
-    console.log(keys[number]);
+    var newIdeaRef = db.child(keys[number] + "/");
 
     //Clear out the contact info if that is displayed from the previous idea
     $("#pair-name").text("");
@@ -59,10 +57,9 @@ function newIdea(number) {
         name = snap.val().name;
         email = snap.val().email;
         idea = snap.val().idea;
+    }).then(function() {
+        $("#idea").text(idea);
     });
-
-    //Set new idea
-    $("#idea").text(idea);
 
     //Reset currentIdea making sure it doesn't excede the keys
     if(currentIdea < keys.length - 1) {
